4e05e4ca0825dcc7c04d0433c4683fa588cf92ad
Rodrigo Kunz <raokrodrigo@gmail.com>
2026-05-26 08:03:26 -0400

    aula 25/05

--- src/main/java/com/projectmanager/ProjectMannage/infrasctructure/dto/exception/AppExceptionHandler.java
+++ src/main/java/com/projectmanager/ProjectMannage/infrasctructure/dto/exception/AppExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.projectmanager.ProjectMannage.infrasctructure.dto.exception;
 
-import jakarta.servlet.Servlet;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,25 +12,38 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class AppExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @ExceptionHandler(value = RequestException.class)
+    public ResponseEntity<Object> handleRequestException(RequestException ex, WebRequest request){
+        return handleException(ex, ex.getErrorcode(), ex.getMessage(), HttpStatus.BAD_REQUEST, request);
+    }
 
     @ExceptionHandler(value = Exception.class)
-    public ResponseEntity<Object> handleGenericExceptions(Exception ex, WebRequest request){
+    public ResponseEntity<Object> handleGenericException(Exception ex, WebRequest request){
+        return handleException(ex, null, ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
 
+    private ResponseEntity<Object> handleException(
+
+            Exception ex,
+            String errorCode,
+            String message,
+            HttpStatus status,
+            WebRequest request
+    ){
         ServletWebRequest servletWebRequest = (ServletWebRequest) request;
 
         return handleExceptionInternal(
                 ex,
                 RestError
                         .builder()
-                        .errorCode("Xyz")
-                        .errorCodeMessage(ex.getMessage())
-                        .status(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+                        .errorCode(errorCode)
+                        .errorCodeMessage(message)
+                        .status(String.valueOf(status.value()))
                         .path(servletWebRequest.getRequest().getRequestURI())
                         .build(),
-                        new  HttpHeaders(),
-                        HttpStatus.BAD_REQUEST,
-                        request
-
+                new HttpHeaders(),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                request
         );
     }
 }
---
+++ src/main/java/com/projectmanager/ProjectMannage/infrasctructure/dto/exception/RequestException.java
@@ -0,0 +1,14 @@
+package com.projectmanager.ProjectMannage.infrasctructure.dto.exception;
+
+import lombok.Getter;
+
+@Getter
+public class RequestException extends RuntimeException {
+    private final String errorCode;
+
+
+    public RequestException(String message, String errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+}
